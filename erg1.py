d8ce7f60677f09f17d504a6e5ce03e0a29ac3cdc
Konstantinos Mousoulis <konmous04@gmail.com>
2025-11-14 21:25:25 +0200

    Second Commit

--- erg1.py
+++ erg1.py
@@ -1,3 +1,43 @@
 import torch
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import time
+
+
+class simpleMLP(nn.Module):
+    def __init__(self, hidden_size=64):
+        super(simpleMLP, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(3 * 32 * 32, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, 10)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465),
+                         (0.2470, 0.2435, 0.2616))
+])
+
+train_dataset = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
+test_dataset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform)
+
+batch_size = 128
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+
+
+model = simpleMLP(hidden_size=64).to(device)
+print(model)
